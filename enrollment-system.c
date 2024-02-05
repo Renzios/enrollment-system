4c750bd1ac8eb15b783907e2a0cb35e22824dfb0
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-02-06 00:19:58 +0800

    feat: main menu

--- enrollment-system.c
+++ enrollment-system.c
@@ -2,5 +2,34 @@
 
 int main(void)
 {
+    int user;
+
+    do
+    {
+        printf("Main Menu\n");
+        printf("1] Student\n");
+        printf("2] Faculty\n");
+        printf("3] Academic Assistant\n");
+        printf("Answer: ");
+        scanf("%d", &user);
+
+        switch (user)
+        {
+            case 1:
+                break;
+                
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            default:
+                break;
+        }
+    }
+    while (true);
+
+
     return 0;
 }
