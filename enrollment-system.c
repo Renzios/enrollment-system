5e8ae5381553fb4bc5a55a8a53d32f219646b4b3
Renzios <61353378+Renzios@users.noreply.github.com.>
2024-02-08 17:44:19 +0800

    feat: student menu

--- enrollment-system.c
+++ enrollment-system.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+void studentMenu();
+
 int main(void)
 {
-    int user;
+    int answer;
 
     do
     {
@@ -10,12 +12,15 @@ int main(void)
         printf("1] Student\n");
         printf("2] Faculty\n");
         printf("3] Academic Assistant\n");
-        printf("Answer: ");
-        scanf("%d", &user);
 
-        switch (user)
+
+        printf("Answer: ");
+        scanf("%d", &answer);
+
+        switch (answer)
         {
             case 1:
+                studentMenu();
                 break;
                 
             case 2:
@@ -32,4 +37,46 @@ int main(void)
 
 
     return 0;
+}
+
+void studentMenu()
+{
+    int studentID;
+
+    do
+    {
+        printf("Enter Student ID: ");
+        scanf("%d", &studentID);
+    }
+    while (studentID < 0);
+
+    int answer;
+
+    do
+    {
+        printf("Choose your transaction:\n");
+        printf("1] Enroll Courses\n");
+        printf("2] Print EAF\n");
+        printf("3] Exit to Return to Main Menu\n");
+
+
+        printf("Answer: ");
+        scanf("%d", &answer);
+
+        switch (answer)
+        {
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            default:
+                break;
+        }
+    }
+    while (transaction != 3);
 }
